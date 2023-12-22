b8b3f53ba7176d938b32a06331dc2ed13fbdff7c
Hiếu Minh <105605227+minhhieu1234@users.noreply.github.com>
2023-12-23 05:38:39 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+
+
+
+import React from "react";
+import SignUp from './view/SignUp';
+import Login from './view/logIn';
+import HomePage from './view/HomePage';
+import NavigationBar from "./view/NavigationBar";
+function App() {
+  return (
+    <div>
+       <HomePage/>
+    </div>
+  
+  );
+}
+
+export default App;
